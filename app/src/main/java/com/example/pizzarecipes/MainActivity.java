fd423005964d6b92a5efac50667dc086d797f299
student_ait409 <talyshevmihail06@gmail.com>
2023-11-08 15:27:09 +0300

    pizza part2

--- app/src/main/java/com/example/pizzarecipes/MainActivity.java
+++ app/src/main/java/com/example/pizzarecipes/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
@@ -14,5 +16,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<PizzaRecipeItem> pizzaRecipeItems = new ArrayList<>();
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA1_TITLE,
+                Utils.PIZZA1_DESCRIPTION,
+                Utils.PIZZA1_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza2,
+                Utils.PIZZA2_TITLE,
+                Utils.PIZZA2_DESCRIPTION,
+                Utils.PIZZA2_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza3,
+                Utils.PIZZA3_TITLE,
+                Utils.PIZZA3_DESCRIPTION,
+                Utils.PIZZA3_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza4,
+                Utils.PIZZA4_TITLE,
+                Utils.PIZZA4_DESCRIPTION,
+                Utils.PIZZA4_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza5,
+                Utils.PIZZA5_TITLE,
+                Utils.PIZZA5_DESCRIPTION,
+                Utils.PIZZA5_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza6,
+                Utils.PIZZA6_TITLE,
+                Utils.PIZZA6_DESCRIPTION,
+                Utils.PIZZA6_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza7,
+                Utils.PIZZA7_TITLE,
+                Utils.PIZZA7_DESCRIPTION,
+                Utils.PIZZA7_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza8,
+                Utils.PIZZA8_TITLE,
+                Utils.PIZZA8_DESCRIPTION,
+                Utils.PIZZA8_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza9,
+                Utils.PIZZA9_TITLE,
+                Utils.PIZZA9_DESCRIPTION,
+                Utils.PIZZA9_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza10,
+                Utils.PIZZA10_TITLE,
+                Utils.PIZZA10_DESCRIPTION,
+                Utils.PIZZA10_RECIPE));
     }
 }
---
+++ app/src/main/java/com/example/pizzarecipes/Utils.java
@@ -0,0 +1,44 @@
+package com.example.pizzarecipes;
+
+public class Utils {
+
+    public static final String PIZZA1_TITLE = "Pizza-angry bird";
+    public static final String PIZZA1_DESCRIPTION = "Very angry bird";
+    public static final String PIZZA1_RECIPE = "Make pizza peperoni\nMake face of angry bird";
+
+    public static final String PIZZA2_TITLE = "Smile pizza";
+    public static final String PIZZA2_DESCRIPTION = "Very happy pizza";
+    public static final String PIZZA2_RECIPE = "Make pizza margarita\nMake smile face";
+
+    public static final String PIZZA3_TITLE = "Spider-pizza";
+    public static final String PIZZA3_DESCRIPTION = "Pizza with red and black spiders";
+    public static final String PIZZA3_RECIPE = "Make pizza 4 cheeses\nMake red and black spiders";
+
+    public static final String PIZZA4_TITLE = "Pizza-hat for cat";
+    public static final String PIZZA4_DESCRIPTION = "Pizza for cat";
+    public static final String PIZZA4_RECIPE = "Make pizza peperoni\nTake 1 slice on cat's head";
+
+    public static final String PIZZA5_TITLE = "Pizza-pig";
+    public static final String PIZZA5_DESCRIPTION = "Pizza with pigs' faces";
+    public static final String PIZZA5_RECIPE = "Make mini-pizzas\nMake face of pigs on them";
+
+    public static final String PIZZA6_TITLE = "Pizza-octopus";
+    public static final String PIZZA6_DESCRIPTION = "Pizza with 8 legs";
+    public static final String PIZZA6_RECIPE = "Make Hawaiian pizza\nMake face of octopus and 8 legs";
+
+    public static final String PIZZA7_TITLE = "Pizza-cat";
+    public static final String PIZZA7_DESCRIPTION = "Very good cat-pizza";
+    public static final String PIZZA7_RECIPE = "Make pizza Diablo\nMake cat's face and ears";
+
+    public static final String PIZZA8_TITLE = "Pizza-heart";
+    public static final String PIZZA8_DESCRIPTION = "Very lovefull pizza";
+    public static final String PIZZA8_RECIPE = "Make pizza peperoni\nMake it in form of heart";
+
+    public static final String PIZZA9_TITLE = "Pizza-monster";
+    public static final String PIZZA9_DESCRIPTION = "Very scary pizza";
+    public static final String PIZZA9_RECIPE = "Make pizza jalapeno\nMake face of scary monster";
+
+    public static final String PIZZA10_TITLE = "Pizza-fish";
+    public static final String PIZZA10_DESCRIPTION = "Pizza with sea creature";
+    public static final String PIZZA10_RECIPE = "Make pizza peperoni\nMake it in form of fish";
+}
